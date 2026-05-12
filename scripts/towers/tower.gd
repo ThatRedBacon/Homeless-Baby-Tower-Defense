acec4912115b4c8cc0883e23b010b1f2dd08bd45
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-05-11 22:23:23 -0400

    Added SEAF tower sprite, modified tower code to display and hide tower defense radius.

--- scripts/towers/tower.gd
+++ scripts/towers/tower.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
-@export var fireRate: float = 1.0
+@export var fireRate: float = 2.0
 @export var range: float = 150.0
 
 var cooldown: float = 0.0
 var target = null
+var isSelected = false
 
-@onready var area = $Area2D
+@onready var attackArea = $AttackArea
+@onready var rangeIndicator = $RangeIndicator
 
+# tower fires on its designated target
 func shoot():
 	if target == null:
 		return
@@ -15,8 +18,30 @@ func shoot():
 	print("Firing...")
 	target.takeDamage(1)
 
+# draws a radius around the tower when clicked on
+func drawRangeIndicator():
+	rangeIndicator.clear_points()
+	
+	var segments = 64
+	for i in range(segments + 1):
+		var angle = (TAU * i) / segments
+		
+		var point = Vector2(
+			cos(angle),
+			sin(angle)
+		) * range
+		
+		rangeIndicator.add_point(point)
+
+# called when user clicks tower. changes tower radius visibility
+func selectTower():
+	isSelected = !isSelected
+	rangeIndicator.visible = isSelected	
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	drawRangeIndicator()
+	rangeIndicator.visible = false	
 	print("Tower ready.")
 
 
@@ -28,12 +53,18 @@ func _process(delta):
 		shoot()
 		cooldown = 1.0 / fireRate
 
-
+# designates a target when one enters its defense radius
 func _on_area_2d_body_entered(body):
 	if body.name == "Enemy":
 		target = body
 
-
+# tower clears its active target when they exit its radius
 func _on_area_2d_body_exited(body):
 	if body == target:
 		target = null
+
+# called when the player clicks on the tower, selecting it
+func _on_click_area_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			selectTower()
