8759eafc25449dc650eab22f5e346ebcfa29ef95
Braeden/Bacon <braeden.lamb.bacon@gmail.com>
2026-05-11 22:50:32 -0400

    Updated tower UI so the displayed and actual defense radii are synced.

--- scripts/towers/tower.gd
+++ scripts/towers/tower.gd
@@ -1,13 +1,19 @@
 extends Node2D
 
 @export var fireRate: float = 2.0
-@export var range: float = 150.0
+@export var range: float = 300.0
+
+# Color(r, g, b, a) with parameters between 0-1
+@export var radiusColor = Color(0.5, 0.5, 1, 0.2)
+@export var radiusBorderColor = Color(0.3, 0.3, 1, 0.8)
+@export var radiusBorderWidth = 3.0
 
 var cooldown: float = 0.0
 var target = null
 var isSelected = false
 
 @onready var attackArea = $AttackArea
+@onready var attackShape = $AttackArea/CollisionShape2D
 @onready var rangeIndicator = $RangeIndicator
 
 # tower fires on its designated target
@@ -18,30 +24,40 @@ func shoot():
 	print("Firing...")
 	target.takeDamage(1)
 
-# draws a radius around the tower when clicked on
-func drawRangeIndicator():
-	rangeIndicator.clear_points()
+# radius draw function
+func _draw():
+	if not isSelected:
+		return
 	
-	var segments = 64
-	for i in range(segments + 1):
-		var angle = (TAU * i) / segments
-		
-		var point = Vector2(
-			cos(angle),
-			sin(angle)
-		) * range
-		
-		rangeIndicator.add_point(point)
+	draw_circle(Vector2.ZERO, range, radiusColor)
+	
+	draw_arc(
+		Vector2.ZERO,
+		range,
+		0,
+		TAU,
+		64,
+		radiusBorderColor,
+		radiusBorderWidth
+	)
+
+# syncs displayed and actual attack range
+func updateRange():
+	var shape = attackShape.shape
+	
+	if shape is CircleShape2D:
+		shape.radius = range
+	
+	queue_redraw()
 
 # called when user clicks tower. changes tower radius visibility
 func selectTower():
 	isSelected = !isSelected
-	rangeIndicator.visible = isSelected	
+	queue_redraw()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	drawRangeIndicator()
-	rangeIndicator.visible = false	
+	updateRange()
 	print("Tower ready.")
 
 
